b3c755e8d4644518bb1723d9d16ebf643fe5ed7d
Niraj gohel <42432442+NirajGohel@users.noreply.github.com>
2023-07-16 15:41:39 +0530

    Implemented center feature

--- routes/CenterRoute.js
+++ routes/CenterRoute.js
@@ -4,9 +4,13 @@ const Center = require("../models/Center")
 
 router.post("/add", async (req, res) => {
     try {
-        //const savedCenter = 
-    } catch (error) {
+        const center = new Center(req.body)
+        center.name = center.name.charAt(0).toUpperCase() + center.name.slice(1)
 
+        const savedCenter = await center.save()
+        res.send(savedCenter)
+    } catch (error) {
+        res.status(400).send(err)
     }
 })
 
@@ -21,9 +25,30 @@ router.post("/getall", async (req, res) => {
 
 router.put("/update", async (req, res) => {
     try {
+        const { id, name, email } = req.body
 
+        let center = await Center.findById({ _id: id })
+        if (!center) return res.status(404).send("Center does not exist!")
+
+        if (center) {
+            if (name && email) {
+                center.name = name.charAt(0).toUpperCase() + name.slice(1)
+                center.email = email
+
+                const updatedCenter = await center.save();
+                if (updatedCenter) {
+                    res.json({ isSuccess: true });
+                }
+                else {
+                    res.json({ isSuccess: false });
+                }
+            }
+            else {
+                res.json({ isSuccess: false, message: 'Insufficient Parameters' })
+            }
+        }
     } catch (error) {
-
+        res.status(400).send(error);
     }
 })
 
